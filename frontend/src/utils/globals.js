7a32fcdad61ec3ccd060c2917893cbee6a111c4b
saurabhraj123 <me4saurabh4u@gmail.com>
2023-04-04 22:18:32 +0530

    mehran credentials for backend used

--- frontend/src/utils/globals.js
+++ frontend/src/utils/globals.js
@@ -1,4 +1,11 @@
-export const BACKEND_URI="http://localhost:5500";
-export const EMAILJS_SERVICE_ID="service_zsulqyv";
-export const EMAILJS_TEMPLATE_ID="template_7raoxuu";
-export const EMAILJS_PUBLIC_KEY="cWM6Fgb7hs57dT2NZ";
+// export const BACKEND_URI="http://localhost:5500";
+// export const EMAILJS_SERVICE_ID="service_zsulqyv";
+// export const EMAILJS_TEMPLATE_ID="template_7raoxuu";
+// export const EMAILJS_PUBLIC_KEY="cWM6Fgb7hs57dT2NZ";
+
+
+// Mehran 
+export const BACKEND_URI="https://mehran-booking-api.com";
+export const EMAILJS_SERVICE_ID="service_m168mdq";
+export const EMAILJS_TEMPLATE_ID="template_15em2r4";
+export const EMAILJS_PUBLIC_KEY="1BeXJJv8dy4YduZmj";
